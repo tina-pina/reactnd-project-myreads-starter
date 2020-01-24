7f86d872b7194b4ec6155aeb30fb65421d56e946
Christina Koenig <christina.koenig@netcentric.biz>
2020-01-24 21:56:45 +0100

    Show bookshelf in search select

--- src/Book.js
+++ src/Book.js
@@ -2,20 +2,19 @@ import React, { Component } from 'react';
 import Modal from './Modal';
 
 class Book extends Component {
-    state = {
-        category: '',
-        bookTitle: '',
-        state: { show: false },
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            show: false,
+            selectedShelf: "",
+        }
     }
 
     handleChangeShelf = newShelf => {
         const { book } = this.props
-        console.log("autor arr ", book.printType)
-        // console.log("bookId: ", book.id, "new shelf: ", newShelf, "current shelf: ", book.shelf)
-        // if(!book.shelf) {
-        //     book.shelf = 'none'
-        // }
-        this.props.handleChangeShelf(book.id, newShelf, book.shelf, book)
+        newShelf = newShelf.target.value
+        this.props.handleChangeShelf(book, newShelf)
     }
 
     showModal = () => {
@@ -25,6 +24,24 @@ class Book extends Component {
     hideModal = () => {
         this.setState({ show: false });
     };
+
+    componentWillMount() {
+        const { book, allBooks } = this.props
+
+        // from main
+        if(!allBooks) {
+            this.setState({selectedShelf: book.shelf})
+            return
+        }
+
+        // from search
+        let selectedBook = allBooks.filter(b => b.id === book.id)[0]
+        if(selectedBook) {
+            this.setState({selectedShelf: selectedBook.shelf})
+        } else {
+            this.setState({selectedShelf: "none"})
+        }
+    }
 
 
     render() {
@@ -37,23 +54,24 @@ class Book extends Component {
                     { book.imageLinks?
                         (<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>): <div>no image available</div>
                     }
-                    <div className="book-shelf-changer">
-                        <select onChange={(ev) => {
-                            this.handleChangeShelf(ev.target.value)}}>
-                            <option value="move">Move to...</option>
+                    <form className="book-shelf-changer">
+                        <select className="book-shelf-changer--select" defaultValue={this.state.selectedShelf} onChange={(ev) => {
+                            this.handleChangeShelf(ev)}}>
+                            <option value="move" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
                             <option value="read">Read</option>
                             <option value="none">None</option>
                         </select>
-                    </div>
+                    </form>
                     </div>
                     <div className="book-title">{ book.title }</div>
                     <div className="book-authors">{ book.authors }</div>
                     <Modal show={this.state.show} handleClose={this.hideModal}>
-                        <p>{book.description.substring(0,400)}...</p>
+                        <p>{book.description}</p>
                     </Modal>
-                    <button class="modal-button" type="button" onClick={this.showModal}>more</button>
+                    <button className="modal-button" type="button" onClick={this.showModal}>more</button>
+                    <p>{this.state.selected}</p>
                 </li>
             </div>
         )
